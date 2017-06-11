9f4c278d203fa0d592520326dfd7877d0febaf8e
Rakita Milos <milosrakita@outlook.com>
2017-06-11 14:51:09 +0200

    milos

--- EAAutoFramework/Helpers/ExcelHelper.cs
+++ EAAutoFramework/Helpers/ExcelHelper.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Excel;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EAAutoFramework.Helpers
 {
-    class ExcelHelper
+    public class ExcelHelper
     {
+        private static DataTable ExcelToDataTable(string fileName)
+        {
+            //open file return as stream
+            FileStream stream = File.Open(fileName, FileMode.Open, FileAccess.Read);
+            //CreateOpenXmlReader <= .xlsx
+            //CreateBinaryReader <= .xls
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            //set first row as column name
+            excelReader.IsFirstRowAsColumnNames = true;
+            //return as DataSet
+            DataSet result = excelReader.AsDataSet();
+            //get all the  tables
+            DataTableCollection table = result.Tables;
+            //store it in DataTable
+            DataTable resultTable = table["Sheet1"];
+            //result
+            return resultTable;
+        }
     }
 }
